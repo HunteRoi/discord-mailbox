221c1512fc06eb24ac40e9feabd99825a7775435
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2023-03-17 08:05:14 +0100

    refactor: use string enum

--- src/ErrorMessages.ts
+++ src/ErrorMessages.ts
@@ -1,15 +1,8 @@
-const noOpenedTicketWithId = '0001 - Provided ticket ID does not exist for any user';
-const tooMuchTicketsOpened = '0002 - Too much tickets for this user';
-const messageHasNoTicket = '0003 - No ticket related to this message';
-const senderNamesAppear = '0004 - Sender names appear although the options is disabled';
-const noMailboxRegistered = '0005 - There is no mailbox registered in the options';
-const guildNotRegistered = '0006 - The provided guild is not registered by the mailbox manager';
-
-export default {
-  noOpenedTicketWithId,
-  tooMuchTicketsOpened,
-  messageHasNoTicket,
-  senderNamesAppear,
-  noMailboxRegistered,
-  guildNotRegistered
-};
+export enum ErrorMessage {
+  noOpenedTicketWithId = '0001 - Provided ticket ID does not exist for any user';
+  tooMuchTicketsOpened = '0002 - Too much tickets for this user';
+  messageHasNoTicket = '0003 - No ticket related to this message';
+  senderNamesAppear = '0004 - Sender names appear although the options is disabled';
+  noMailboxRegistered = '0005 - There is no mailbox registered in the options';
+  guildNotRegistered = '0006 - The provided guild is not registered by the mailbox manager';
+}
