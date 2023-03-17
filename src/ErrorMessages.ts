cbbb9b1bf6cd597a98d627ce6912cc51331b5534
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2023-03-17 16:00:37 +0100

    fix: properly export ErrorMessages enum

--- src/ErrorMessages.ts
+++ src/ErrorMessages.ts
@@ -1,4 +1,4 @@
-export default enum ErrorMessages {
+enum ErrorMessages {
   noOpenedTicketWithId = '0001 - Provided ticket ID does not exist for any user',
   tooMuchTicketsOpened = '0002 - Too much tickets for this user',
   messageHasNoTicket = '0003 - No ticket related to this message',
@@ -6,3 +6,5 @@ export default enum ErrorMessages {
   noMailboxRegistered = '0005 - There is no mailbox registered in the options',
   guildNotRegistered = '0006 - The provided guild is not registered by the mailbox manager',
 }
+
+export default ErrorMessages;
