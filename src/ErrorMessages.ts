0a4898cbbcb8f6962456984ab7b4af3c81f56857
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2023-03-17 15:54:09 +0100

    use coma instead of semicolon

--- src/ErrorMessages.ts
+++ src/ErrorMessages.ts
@@ -1,8 +1,8 @@
 export default enum ErrorMessages {
-  noOpenedTicketWithId = '0001 - Provided ticket ID does not exist for any user';
-  tooMuchTicketsOpened = '0002 - Too much tickets for this user';
-  messageHasNoTicket = '0003 - No ticket related to this message';
-  senderNamesAppear = '0004 - Sender names appear although the options is disabled';
-  noMailboxRegistered = '0005 - There is no mailbox registered in the options';
-  guildNotRegistered = '0006 - The provided guild is not registered by the mailbox manager';
+  noOpenedTicketWithId = '0001 - Provided ticket ID does not exist for any user',
+  tooMuchTicketsOpened = '0002 - Too much tickets for this user',
+  messageHasNoTicket = '0003 - No ticket related to this message',
+  senderNamesAppear = '0004 - Sender names appear although the options is disabled',
+  noMailboxRegistered = '0005 - There is no mailbox registered in the options',
+  guildNotRegistered = '0006 - The provided guild is not registered by the mailbox manager',
 }
